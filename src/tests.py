cd2ccbfe39f5425477bb80a291c56780535f59c7
Diogo Santos <drsantos989@gmail.com>
2021-07-05 18:24:19 +0100

    add test.py

---
+++ src/tests.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_sim():
+    return None
+
+def test_return():
+    return None
+
+def test_annualized_returns():
+    return None
+
+def test_lump_sum():
+    return None
+
+def test_dca():
+    return None
